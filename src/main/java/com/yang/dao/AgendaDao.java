97ab1d6142059e0e98356db1582dd118257caa3f
matrix-yang <739260804@qq.com>
2017-09-20 15:18:19 +0800

    完善dao层的增删改

--- src/main/java/com/yang/dao/AgendaDao.java
+++ src/main/java/com/yang/dao/AgendaDao.java
@@ -3,30 +3,42 @@ package com.yang.dao;
 import com.sun.org.apache.bcel.internal.generic.NEW;
 import com.yang.model.Agenda;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Administrator on 2017/9/19.
  */
 public class AgendaDao {
-    private List<Agenda> agendas;
+    private List<Agenda> agendas = new ArrayList<Agenda>();
     private static final AgendaDao agendaDao = new AgendaDao();
 
     private AgendaDao() {
     }
 
-    public static AgendaDao getInstance(){
+    public static AgendaDao getInstance() {
         return agendaDao;
     }
 
-    public void init(List<Agenda> agendas){
-        this.agendas=agendas;
-    }
-
-    public Agenda findByTitle(String title){
-        for (Agenda agenda:agendas){
+    public Agenda findByTitle(String title) {
+        for (Agenda agenda : agendas) {
             if (agenda.getTille().equals(title)) return agenda;
         }
         return null;
+    }
+
+    public boolean addAgenda(Agenda agenda) {
+        agendas.add(agenda);
+        return true;
+    }
+
+    public boolean deleteAgenda(String title) {
+        for (Agenda agenda : agendas) {
+            if (agenda.getTille().equals(title)) {
+                agendas.remove(agenda);
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/com/yang/dao/UserDao.java
+++ src/main/java/com/yang/dao/UserDao.java
@@ -1,29 +1,43 @@
 package com.yang.dao;
 
 import com.yang.model.User;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Administrator on 2017/9/19.
  */
 public class UserDao {
-    private List<User> users;
-    private static final UserDao userDao=new UserDao();
+    private List<User> users = new ArrayList<User>();
+    private static final UserDao userDao = new UserDao();
 
-    private UserDao(){}
+    private UserDao() {
+    }
 
-    public static UserDao getInstance(){
+    public static UserDao getInstance() {
         return userDao;
     }
 
-    public void init(List<User> users){
-        this.users=users;
-    }
-
-    public User findByName(String name){
-        for (User user:users){
+    public User findByName(String name) {
+        for (User user : users) {
             if (user.getUserName().equals(name)) return user;
         }
         return null;
+    }
+
+    public boolean addUser(User user) {
+        users.add(user);
+        return true;
+    }
+
+    public boolean deleteUser(String userName) {
+        for (User user : users) {
+            if (user.getUserName().equals(userName)) {
+                users.remove(user);
+                return true;
+            }
+        }
+        return false;
     }
 }
